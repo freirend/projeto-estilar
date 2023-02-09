29bae18c79c4e7e9bab49d4f87ca03201a1bea86
Lucas Freire <lucfre7@gmail.com>
2023-02-09 11:48:06 -0300

    att

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const burguer = document.querySelector('.uil-bars');
 const navlist = document.querySelector('#nav')
 const navbtn = document.querySelectorAll('.nav-btn')
+const valueCount = document.querySelectorAll('.count')
+const interval = 5000;
+
 
 burguer.addEventListener('click', ()=>{
     if (burguer.className == 'uil uil-bars' || burguer.className == 'uil-bars'){
@@ -23,3 +26,16 @@ for (c = 0; c < navbtn.length; c++){
     var busca = navbtn[c].id
     captarElementos(busca)
 }
+
+valueCount.forEach((count) => {
+    var startCount = 0;
+    var endCount = Number(count.getAttribute("data-count"));
+    var duration = Math.floor(interval / endCount);
+    var counter = setInterval(function(){
+        startCount += 1;
+        count.textContent = startCount;
+        if (startCount == endCount){
+            clearInterval(counter);
+        }
+    }, duration)
+})
